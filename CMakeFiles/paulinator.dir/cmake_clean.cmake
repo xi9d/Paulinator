109b389d1a4b2c52045aa6f3a81a832578d03379
Paul webo <toursitp@gmail.com>
2024-11-20 23:30:46 +0300

    configured glfw

---
+++ CMakeFiles/paulinator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paulinator.dir/main.cpp.obj"
+  "CMakeFiles/paulinator.dir/main.cpp.obj.d"
+  "libpaulinator.dll.a"
+  "paulinator.exe"
+  "paulinator.exe.manifest"
+  "paulinator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paulinator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
